271bade9cd398a0bf1a13757a51274151b540da0
Lukas Sinkar <lukassinkarofficial@gmail.com>
2020-05-14 02:43:06 +0300

    added register functionality

--- src/sample/Controller/Controller.java
+++ src/sample/Controller/Controller.java
@@ -3,12 +3,16 @@ package sample.Controller;
 import sample.Model.User;
 import sample.Model.UserDAO;
 import sample.Utils.Validation;
-
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.stage.Stage;
 
 import java.sql.ResultSet;
 
@@ -20,6 +24,32 @@ public class Controller {
     private TextField username;
     @FXML
     private Label error;
+    @FXML
+    private Button close;
+    @FXML
+    private TextField regUser;
+    @FXML
+    private PasswordField regPassw;
+    @FXML
+    private PasswordField regConfPassw;
+    @FXML
+    private TextField regEmail;
+    @FXML
+    private Label regError;
+    @FXML
+    private CheckBox cbHorror;
+    @FXML
+    private CheckBox cbRomance;
+    @FXML
+    private CheckBox cbAction;
+    @FXML
+    private RadioButton rbNetflix;
+    @FXML
+    private RadioButton rbHulu;
+    @FXML
+    private RadioButton rbDisney;
+    @FXML
+    private CheckBox admin;
 
     ResultSet rsAllEntries;
     ObservableList<ObservableList> data = FXCollections.observableArrayList();
@@ -39,5 +69,80 @@ public class Controller {
             error.setText("Wrong user name or password!");
         }
         error.setVisible(true);
+    }
+
+    public void logOut(ActionEvent event) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("../view/login.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("Login");
+            stage.setScene(new Scene(root, 450, 350));
+            stage.show();
+            ((Node) (event.getSource())).getScene().getWindow().hide();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // button 'X' closes stage(window)
+    public void closeWindow(ActionEvent event) {
+        if (event.getSource() == close) {
+            System.exit(0);
+        }
+    }
+
+    public void register(ActionEvent event) {
+        try {
+            // we are in controller folder, but our view is not here, so we need to go one step up - ../
+            Parent root = FXMLLoader.load(getClass().getResource("../view/register.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("Register");
+            stage.setScene(new Scene(root, 450, 350));
+            stage.show();
+            ((Node) (event.getSource())).getScene().getWindow().hide();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void registerLogin(ActionEvent event) {
+        boolean isRegistered = true;
+
+        // clear errors on btn pressed
+        regError.setText("");
+        if (!Validation.isValidUsername(regUser.getText())) {
+            regError.setText("Username is incorrect (letters and numbers only, at least 5 char)");
+            isRegistered = false;
+        } else if (!Validation.isValidPassword(regPassw.getText())) {
+            regError.setText("Password is incorrect (letters and numbers only, at least 5 char)");
+            isRegistered = false;
+        } else if (!regConfPassw.getText().equals(regPassw.getText())) {
+            regError.setText("Password doesn't match");
+            isRegistered = false;
+        } else if (!Validation.isValidEmail(regEmail.getText())) {
+            regError.setText("Email is not correct, pattern- dakar@one.lt");
+            isRegistered = false;
+        }
+
+        if (isRegistered) {
+            User user = new User(regUser.getText(), regPassw.getText(), regEmail.getText(), admin.isSelected());
+            UserDAO userDAO = new UserDAO();
+            String msg = userDAO.register(user);
+            if (msg.contains("successfully")) {
+                try {
+                    Parent root = FXMLLoader.load(getClass().getResource("../view/login.fxml"));
+                    Stage stage = new Stage();
+                    stage.setTitle("Login");
+                    stage.setScene(new Scene(root, 450, 350));
+                    stage.show();
+                    // hides current stage (window)
+                    ((Node) (event.getSource())).getScene().getWindow().hide();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } else {
+                regError.setText(msg);
+            }
+        }
     }
 }
--- src/sample/Utils/Validation.java
+++ src/sample/Utils/Validation.java
@@ -7,6 +7,9 @@ public class Validation {
 
     public static final String USER_NAME_REGEX_PATTERN = "^[a-zA-z0-9]{5,12}$";
     public static final String USER_PASSWORD_REGEX_PATTERN = "^[a-zA-z0-9@!?_#&%.^]{5,12}$";
+    public static final String USER_EMAIL_REGEX_PATTERN = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,50}$";
+    public static final String TITLE_REGEX_PATTERN = "^[a-zA-Z0-9-_!@#$ ]{5,55}$";
+    public static final String AUTHOR_REGEX_PATTERN = "^[a-zA-Z0-9 ]{5,55}$";
 
     public static boolean isValidUsername(String user_name) {
         Pattern pattern = Pattern.compile(USER_NAME_REGEX_PATTERN);
@@ -17,6 +20,12 @@ public class Validation {
     public static boolean isValidPassword(String password) {
         Pattern pattern = Pattern.compile(USER_PASSWORD_REGEX_PATTERN);
         Matcher matcher = pattern.matcher(password);
+        return matcher.find();
+    }
+
+    public static boolean isValidEmail(String regEmail) {
+        Pattern pattern = Pattern.compile(USER_EMAIL_REGEX_PATTERN);
+        Matcher matcher = pattern.matcher(regEmail);
         return matcher.find();
     }
 }
